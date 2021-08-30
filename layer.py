1638267fa680561247b0e30c4a58a61d01d623fd
martin.gleason.ms@gmail.com <martin.gleason.ms@gmail.com>
2021-08-30 14:45:33 -0500

    geocoder built.

--- layer.py
+++
@@ -1,9 +0,0 @@
-import os, sys, arcpy
-
-#first test: read layer file and print what's in it
-
-locator_path = r"C:\Users\Public\ArcGIS\ccjpdLocator\MyProject1\Police Districts Chicago.lyrx"
-
-lyrFile = arcpy.mp.LayerFile(locator_path)
-for lyr in lyrFile.listLayers():
-    print(lyr.name)
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import os, sys, arcpy
 from test_addresses import addresses
 from geocoder import Geocoder
 
 gc = Geocoder()
 address = addresses
 
-print(address[1].get('address'))
+terry_ln = gc.geocode_address(address[1].get('address'))
 
-gc.geocode_to_district(address = address[1].get('address'))
+print(gc.return_district(terry_ln, verbose = True))
 
-#gc.geocode_address()
+print(gc.geocode_to_district(address[0].get('address')))
+
+print('district is: ' + str(gc.geocode_to_district('5410 S Wood, Chicago, Il')))
+
+gc.geocode_to_district('5410 S Wood, Chicago, Il')
