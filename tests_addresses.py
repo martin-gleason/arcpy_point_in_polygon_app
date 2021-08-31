6b49b8d6ebf529aa83bf320435f52cd07c97a07a
Martin Gleason <officergleason@gmail.com>
2021-08-31 08:27:34 -0500

    merge from yesterday to local

--- tests_addresses.py
+++ tests_addresses.py
@@ -6,7 +6,7 @@ addresses = [
   {'address': '736 terry lane, Countryside, IL, 60525'},
   {'address': '4936 s Forrestville, Chicago, IL 60622'},
   {'address': '2245 w ogden ave, chicago, il 60612'},
-  {'address': '5151 North Milwaukee Ave, chicago, il'}, #16th District
+  {'address': '5151 North Milwaukee Ave, chicago, il'}, ##16th District
   {'address': '3261 W. Maypole, Chicago IL 60624'}, # 11th District
   {'address': '7373 E Iowa Avenue, Denver CO 80231'}
 
