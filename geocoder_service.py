b022f208f4542c4c8438fe200d0b1b57efb68e9d
Martin Gleason <marty.data.scientist@icloud.com>
2021-09-08 16:11:36 -0500

    two end points added

--- geocoder_service.py
+++ geocoder_service.py
@@ -1,59 +1,41 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
 import unit_structure as office
+import json
 import geocoder
 
+#load geocoder
 gc = geocoder.Geocoder()
 
-office = office.load_units_to_class('units\list_of_units.json')
+#load city units
+#turns json file to list
+city_units = office.load_units_to_class('units\list_of_units.json')
 
+
+#load app
 app = Flask(__name__)
+api = Api(app)
 
+class Units(Resource):
+    def get(self):
+        unit_list  = []
+        for unit in city_units:
+            unit_list.append(unit.__dict__)
+        return unit_list
 
-
-class Item(Resource):
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None) is not None:
-            return {'message': "An item with name '{}' lready exists.".format(name)}, 400
-
-        data = request.get_json()
-        items.append(item)
-        return item
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted.'}
-
-
-    def put(self, name):
-        parser = reqparse.RequestParser() #can go through fields in a form like in json payload
-        parser.add_argument('price', 
-            type = float,
-            required=True,
-            help='This Field cannot be left blank!'
-        )
-
-        data = parser.parse_args()
-
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else: 
-            item.update(data)
-        return item
+class GetDistrict(Resource):
+    def get(self):
+        pd = []
+        for unit in city_units:
+            pd.append(unit.get_police_district())
+        return pd
         
 
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
+# class GeoService(Resource, gc):
+#     def get
 
 
-
+api.add_resource(Units, '/units')
+api.add_resource(GetDistrict, '/districts')
 app.run(port=5000, debug=True)
 
--- test_flask.py
+++ test_flask.py
@@ -1,6 +1,8 @@
-from flask import Flask
-import arcgis
-import geocoder
+import unit_structure as office
 
-gc = geocoder.Geocoder()
-print(gc)
+
+#load city units
+city_units = office.load_units_to_class('units\list_of_units.json')
+
+print( "unit": city_units)
+
